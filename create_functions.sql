7a76e66668a47597554c8cc42f2f4c21d38ee95f
Tobiasz Szulc <szulc.tobiasz@gmail.com>
2022-01-14 17:10:43 +0100

    added some functions and a procedure

--- create_functions.sql
+++ create_functions.sql
@@ -1,4 +1,4 @@
-create function Produkty_szczegóły_w_danym_momencie_w_czasie (@when date)
+create function Produkty_szczegóły_w_danym_momencie_w_czasie (@when datetime)
 RETURNS table
 as Return (
     with tab as (
@@ -9,3 +9,73 @@ as Return (
     left outer join tab PS2 on PS1.data_wprowadzenia<PS2.data_wprowadzenia
     where PS2.data_wprowadzenia is null
     )
+
+create function Stałe_w_danym_momencie_w_czasie (@when datetime)
+RETURNS table
+as Return (
+    with tab as (
+        select * from Stałe
+        where  data_wprowadzenia < @when
+    )
+    select PS1.* from tab PS1
+    left outer join tab PS2 on PS1.data_wprowadzenia<PS2.data_wprowadzenia
+    where PS2.data_wprowadzenia is null
+    )
+
+
+create function Menu_w_danym_momencie_w_czasie (@when datetime)
+RETURNS table
+as Return (
+    with tab as (
+        select * from Menu
+        where  data_wprowadzenia < @when
+    )
+    select PS1.* from tab PS1
+    left outer join tab PS2 on PS1.data_wprowadzenia<PS2.data_wprowadzenia
+    where PS2.data_wprowadzenia is null
+    )
+
+
+create function Stoliki_szczegóły_w_danym_momencie_w_czasie (@when datetime)
+RETURNS table
+as Return (
+    with tab as (
+        select * from Stoliki_szczegóły
+        where  data_wprowadzenia < @when
+    )
+    select PS1.* from tab PS1
+    left outer join tab PS2 on PS1.data_wprowadzenia<PS2.data_wprowadzenia
+    where PS2.data_wprowadzenia is null
+    )
+
+create function Procent_rabatu (@id_rabatu int)
+RETURNS float
+as begin
+    declare @when datetime
+    if @id_rabatu is null
+        return 0
+    select @when = data_przyznania from Przyznane_rabaty where id_rabatu = @id_rabatu
+
+    if @id_rabatu in (select id_rabatu from Przyznane_rabaty_typu_2)
+        return (select R2 from Stałe_w_danym_momencie_w_czasie(@when))
+    if @id_rabatu in (select id_rabatu from Przyznane_rabaty_typu_manager)
+        return(select procent from Przyznane_rabaty_typu_manager where id_rabatu = @id_rabatu)
+    if @id_rabatu in (select id_rabatu from Przyznane_rabaty_typu_1)
+        return(select R1 from Stałe_w_danym_momencie_w_czasie(@when))
+    return 0
+end
+
+
+create function Wartość_zamówienia (@id_zamówienia int)
+RETURNS money
+as begin
+    declare @when datetime
+    declare @procent float
+    select @when = data_złorzenia_zamówienia from Zamówienia where id_zamówienia=@id_zamówienia
+    select @procent = dbo.Procent_rabatu(id_rabatu) from Zamówienia where id_zamówienia=@id_zamówienia
+    return (select sum(Produkty_szczegóły_w_danym_momencie_w_czasie.cena * Zs.ilość* (1.0-@procent)) from Zamówienia_szczegóły Zs
+        join Produkty P on P.id_produktu = Zs.id_produktu
+        join Produkty_szczegóły_w_danym_momencie_w_czasie(@when) on Produkty_szczegóły_w_danym_momencie_w_czasie.id_produktu=P.id_produktu
+        where id_zamówienia= @id_zamówienia)
+end
+
---
+++ create_procedures.sql
@@ -0,0 +1,13 @@
+create procedure Wystaw_fakture_miesieczną(@id_klienta int , @month date,@id_pracownika int)
+as
+begin
+    declare @id_faktury int
+    insert into Faktury values (@id_klienta,current_timestamp,1,@id_pracownika)
+    select @id_faktury=SCOPE_IDENTITY()
+    insert into Faktura_szczegóły
+        select @id_faktury,Zamówienia.id_zamówienia from Zamówienia
+        left outer join Faktura_szczegóły Fs on Zamówienia.id_zamówienia = Fs.id_zamówienia
+        where YEAR(@month) = YEAR(data_złorzenia_zamówienia)
+          and Month(@month) = MONTH(data_złorzenia_zamówienia)
+          and id_faktury is null
+end
